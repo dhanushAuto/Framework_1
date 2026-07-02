806d79322e4069d1e97af7f30b992ec02f1ee678
DhanushKodi Velliangiri <dhanush.testing100@gmail.com>
2026-07-03 00:53:36 +0530

    jenkinsfile updates

--- src/test/java/API_Runner/SmokeRunner.java
+++ src/test/java/API_Runner/SmokeRunner.java
@@ -2,11 +2,22 @@ package API_Runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-    features = "src/test/resources/API_Features/GET",
-    glue = "API_StepDef",
-    plugin = {"json:target/cucumber.json", "html:target/cucumber.html"}
+        features = "src/test/resources/API_Features/GET",
+        glue = "API_StepDef",
+        plugin = {
+                "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json"
+        }
 )
 public class SmokeRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
